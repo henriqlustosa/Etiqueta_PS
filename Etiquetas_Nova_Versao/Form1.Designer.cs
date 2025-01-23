9881dcb0149fd6b84e77deae2f1578a357224ff1
henriqlustosa <henriqlustosa@gmail.com>
2025-01-23 14:21:47 -0300

    Atualizado a aplicacao de etiqueta para o PSI para atender o nome social e nome do pai

--- Etiquetas_Nova_Versao/Form1.Designer.cs
+++ Etiquetas_Nova_Versao/Form1.Designer.cs
@@ -118,11 +118,11 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(195, 49);
+            this.label3.Location = new System.Drawing.Point(146, 50);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(253, 25);
+            this.label3.Size = new System.Drawing.Size(323, 25);
             this.label3.TabIndex = 14;
-            this.label3.Text = "Etiquetas Pronto Socorro";
+            this.label3.Text = "Etiquetas Pronto Socorro Infantil";
             // 
             // label4
             // 
@@ -149,7 +149,7 @@
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(65, 13);
             this.label6.TabIndex = 17;
-            this.label6.Text = "14/01/2025";
+            this.label6.Text = "23/01/2025";
             // 
             // Form1
             // 
--- Etiquetas_Nova_Versao/Form1.cs
+++ Etiquetas_Nova_Versao/Form1.cs
@@ -1,37 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Net;
 using System.Text;
 using System.Windows.Forms;
-using System.Net;
 using Newtonsoft.Json;
-using System.IO;
 using System.Drawing.Printing;
 
 namespace Etiquetas_Nova_Versao
 {
     public partial class Form1 : Form
     {
-       
+        private int status;
+        private Paciente paciente;
+        private string error;
 
-        int status;
-        Paciente paciente;
-       // Internacao internacao;
-      //  List<Internacao> internacoes;
-        string error;
-        //string Andar ="";
-        ///string Quarto ="";
-        //string Leito ="";
-       // string Clinica="";
-        int starty = 10;//distancia das linhas
+        // Distância inicial das linhas (top padding).
+        private const int START_Y = 5;
+        private const int START_X = 10;
 
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
-
-        int startXEsquerda = 15;
-     
         public class Paciente
         {
             public string nm_consulta { get; set; }
@@ -42,121 +35,75 @@ namespace Etiquetas_Nova_Versao
             public string sexo { get; set; }
             public string nm_mae_paciente { get; set; }
             public string nm_pai_paciente { get; set; }
-           
-        }
-      /*  public class Internacao
-        {
-            public string cd_prontuario { get; set; }
-            public string nr_leito { get; set; }
-            public string dt_alta_medica { get; set; }
-            public string nm_especialidade { get; set; }
-
-        }*/
-        public Form1()
-        {
-            InitializeComponent();
-
-         
         }
 
-
+        #region Button Events
 
         private void btImprimir_Click(object sender, EventArgs e)
         {
+            string rh = txbRh.Text.Trim();
+
+            // .NET 3.5 doesn't have IsNullOrWhiteSpace:
+            if (string.IsNullOrEmpty(rh) || rh.Trim().Length == 0)
+            {
+                MessageBox.Show("Por favor, digite um número de RH válido.");
+                return;
+            }
+
             try
             {
+                // 1. Busca dados do paciente
+                paciente = FetchPacienteData(rh);
 
-                string rh = txbRh.Text;
-                //detiq = dados.getDados(be);
-                string url = "http://10.48.17.99:5003/hspmsgh-api/pacientes/consulta/" + rh;
-                WebRequest request = WebRequest.Create(url);
-                try
+                if (paciente == null)
                 {
-                    using (var twitpicResponse = (HttpWebResponse)request.GetResponse())
-                    {
-                        using (var reader = new StreamReader(twitpicResponse.GetResponseStream()))
-                        {
-                            JsonSerializer json = new JsonSerializer();
-                            var objText = reader.ReadToEnd();
-                            paciente = JsonConvert.DeserializeObject<Paciente>(objText);
-
-                        }
-                    }
-
-                /*    string url2 = "http://10.48.21.64:5003/hspmsgh-api/internacoes/" + rh;
-                    WebRequest request2 = WebRequest.Create(url2);
-                    using (var twitpicResponse2 = (HttpWebResponse)request2.GetResponse())
-                    {
-                        using (var reader2 = new StreamReader(twitpicResponse2.GetResponseStream()))
-                        {
-                            JsonSerializer json2 = new JsonSerializer();
-                            var objText2 = reader2.ReadToEnd();
-                            internacoes = JsonConvert.DeserializeObject<List<Internacao>>(objText2);
-
-                        }
-                    }
-
-                    if (internacoes.Count != 0)
-                    {
-                        if (internacoes[0].dt_alta_medica == null)
-
-                        Andar = internacoes[0].nr_leito == null ? "" : internacoes[0].nr_leito.Substring(0, 2);
-                        Quarto = internacoes[0].nr_leito == null ? "" : internacoes[0].nr_leito.Substring(2, 2);
-                        Leito = internacoes[0].nr_leito == null ? "" : internacoes[0].nr_leito.Substring(5, 2);
-                        Clinica = internacoes[0].nm_especialidade == null ? "" : internacoes[0].nm_especialidade;
-
-
-                    }
-
-                    */
-
-                   // PrintDialog printDialog1 = new PrintDialog();
-//printDialog1.Document = printDocument1;  
-                  
-                    printDocument1.PrinterSettings.PrinterName = "HP TI";
-                  
-                   
-                        
-                    { 
-                        
-                           
-                        
-                          
-                        int i = 0;
-                        int size =Convert.ToInt32( comboBox1.SelectedItem);
-                        
-                            while (i < size)
-                            {
-                                printDocument1.Print();
-                                i++;
-                            }
-                     
-                    }
-
-
-                    txbRh.Text = "";
-                  //  Andar = "";
-                  //  Quarto = "";
-                   // Leito = "";
-                   // Clinica = "";
-
-
-
-            }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Número de RH inexistente! " + ex.Message);
-                    status = 1;
-
+                    // Caso a requisição retorne nulo ou não tenha encontrado o paciente
+                    MessageBox.Show("Paciente com RH " + rh + " não encontrado.");
+                    return;
                 }
 
+                // 2. Configura a impressora
+                printDocument1.PrinterSettings.PrinterName = "EtiquetaPSI";
+
+                // 3. Lê a quantidade de impressões desejadas
+                object selectedItem = comboBox1.SelectedItem;
+                if (selectedItem == null)
+                {
+                    MessageBox.Show("Selecione uma quantidade de etiquetas válida.");
+                    return;
+                }
+
+                int size;
+                // .NET 3.5 supports int.TryParse:
+                if (!int.TryParse(selectedItem.ToString(), out size) || size <= 0)
+                {
+                    MessageBox.Show("Selecione uma quantidade de etiquetas válida.");
+                    return;
+                }
+
+                // 4. Realiza a impressão múltipla
+                for (int i = 0; i < size; i++)
+                {
+                    printDocument1.Print();
+                }
+
+                // 5. Limpa o RH
+                txbRh.Clear();
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Número de RH inexistente! " + ex.Message);
+                MessageBox.Show("Erro durante a operação: " + ex.Message);
                 status = 1;
-
             }
+        }
+
+        #endregion
+
+        #region BackgroundWorker Events (if needed)
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Caso utilize o backgroundWorker no futuro
         }
 
         private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
@@ -166,152 +113,149 @@ namespace Etiquetas_Nova_Versao
                 lblError.Text = error;
             else
                 lblError.ResetText();
-
-
-
         }
 
-        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        #endregion
+
+        #region Printing
+
+        private void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
         {
+            // -- Paper size em centésimos de polegada (197 x 120 => 1.97" x 1.20") --
+            e.PageSettings.PaperSize = new PaperSize("Custom2", 197, 120);
 
-        }
-
-
-
-        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
-        {
-            string nomep = "";
-            string nomeCompos = "";
-            string nomem = "";
-            string nomeComposM = "";
-            string nm_nome_social = paciente.nm_social;
-            string nm_nome = paciente.nm_paciente;
-            string nm_nome_mae = paciente.nm_mae_paciente;
-            string nm_nome_pai = paciente.nm_pai_paciente;
-            if (!nm_nome_social.Equals(""))
-            {
-                nm_nome = nm_nome_social + " (" + nm_nome + ")";
-            }
-
-            if (nm_nome.Length > 40)
-            {
-             nomep = nm_nome.Substring(0, 40);
-             nomeCompos = nm_nome.Substring(40);
-            }
-            if (nm_nome_mae.Equals("DESCONHECIDA"))
-            {
-                nm_nome_mae = nm_nome_pai;
-            }
-            if (nm_nome_mae.Length > 40)
-            {
-                nomem = nm_nome_mae.Substring(0, 40);
-                nomeComposM = nm_nome_mae.Substring(40);
-            }
-
-
-            //DateTime dt = DateTime.Now;
-            //string data = dt.ToString("g");  
-
-            e.PageSettings.PaperSize = new System.Drawing.Printing.PaperSize("Custom2", 25, 100);//900 é a largura da página
-            //printDocument1.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("Custom2", 500, 1000);
-           // printDialog1.PrinterSettings.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("Custom2", 500, 1000);
             Graphics g = e.Graphics;
-           
-           
 
+            // Constrói o nome do paciente levando em conta o "nm_social"
+            string nomeCompleto = BuildNomeCompleto(
+                paciente.nm_paciente,
+                paciente.nm_social
+            );
 
-            
+            // Se "DESCONHECIDA", usa o nome do pai
+            string nomeMae = (paciente.nm_mae_paciente == "DESCONHECIDA")
+                ? paciente.nm_pai_paciente
+                : paciente.nm_mae_paciente;
 
-                   
+            // Desenha as linhas de texto
+            float currentY = START_Y;
 
-                    if (nm_nome.Length > 40)
-                    {
+            // Linha 1
+            currentY = DrawLine(g, "BE: " + paciente.nm_consulta, currentY, 8, false);
 
+            // Linha 2
+            currentY = DrawLine(g, "Data: " + paciente.check_in, currentY, 8, false);
 
+            // Linha 3 e 4 (Nome paciente)
+            // Nome pode ser grande, então tratamos em duas linhas se ultrapassar 20 caracteres
+            currentY = DrawWrappedLine(g, "Nome: ", nomeCompleto, currentY);
 
+            // Linha seguinte - Nascimento e Sexo
+            currentY = DrawLine(g, "Nasc: " + paciente.data_nascimento + "  Sexo: " + paciente.sexo, currentY, 8, false);
 
-                      
-                        g.DrawString("Nº BE: " + paciente.nm_consulta , new Font("Arial", 8, FontStyle.Regular), System.Drawing.Brushes.Black, startXEsquerda, starty );
-                        g.DrawString("Data: " + paciente.check_in, new Font("Arial", 8, FontStyle.Regular), System.Drawing.Brushes.Black, startXEsquerda, starty +15);
-                        g.DrawString("Nome: " + nomep, new Font("Arial", 8, FontStyle.Bold), System.Drawing.Brushes.Black, startXEsquerda, starty + 30);
-                        
-                        
-                        g.DrawString( nomeCompos, new Font("Arial", 8, FontStyle.Bold), System.Drawing.Brushes.Black, startXEsquerda, starty + 45);
-                      
-                       g.DrawString("Nasc: " + paciente.data_nascimento    + "  Sexo: " + paciente.sexo, new Font("Arial", 8 , FontStyle.Regular), System.Drawing.Brushes.Black, startXEsquerda, starty + 60);
-
-
-
-
-                       if (paciente.nm_mae_paciente.Length > 40)
-                       {
-                           g.DrawString("Filiação: " + nomem, new Font("Arial", 8, FontStyle.Regular), System.Drawing.Brushes.Black, startXEsquerda, starty + 75);
-
-
-                           g.DrawString(nomeComposM, new Font("Arial", 8, FontStyle.Regular), System.Drawing.Brushes.Black, startXEsquerda, starty + 90);
-                       }
-                       else {
-
-                           g.DrawString("Filiação: " + nm_nome_mae, new Font("Arial", 8, FontStyle.Regular), System.Drawing.Brushes.Black, startXEsquerda, starty + 75);
-
-                       }
-
-                      
-
-
-                      
-
-                    }
-                    else
-                    {
-                       
-                        g.DrawString("BE: " + paciente.nm_consulta , new Font("Arial", 8, FontStyle.Regular), System.Drawing.Brushes.Black, startXEsquerda, starty );
-                        g.DrawString("Data: " + paciente.check_in, new Font("Arial", 8, FontStyle.Regular), System.Drawing.Brushes.Black, startXEsquerda, starty + 15);
-                        g.DrawString("Nome: " + nm_nome, new Font("Arial", 8, FontStyle.Bold), System.Drawing.Brushes.Black, startXEsquerda, starty + 30);
-
-                      
-
-                        g.DrawString("Nasc: " + paciente.data_nascimento + "     Sexo: " + paciente.sexo, new Font("Arial", 8, FontStyle.Regular), System.Drawing.Brushes.Black, startXEsquerda, starty + 45);
-
-                        if (paciente.nm_mae_paciente.Length > 40)
-                        {
-                            g.DrawString("Filiação: " + nomem, new Font("Arial", 8, FontStyle.Regular), System.Drawing.Brushes.Black, startXEsquerda, starty + 60);
-
-
-                            g.DrawString(nomeComposM, new Font("Arial", 8, FontStyle.Regular), System.Drawing.Brushes.Black, startXEsquerda, starty + 75);
-                        }
-                        else
-                        {
-
-                            g.DrawString("Filiação: " + nm_nome_mae, new Font("Arial", 8, FontStyle.Regular), System.Drawing.Brushes.Black, startXEsquerda, starty + 60);
-
-                        }
-
-                      
-                     
-
-
-                  
-                    }
-                 
-            
-
-            
-
-
+            // Filiação
+            currentY = DrawWrappedLine(g, "Filiação: ", nomeMae, currentY);
         }
+
+        /// <summary>
+        /// Desenha uma linha simples de texto e retorna a nova posição Y (abaixo da linha).
+        /// </summary>
+        private float DrawLine(Graphics g, string text, float currentY, int fontSize, bool isBold)
+        {
+            using (var font = new Font("Arial", fontSize, isBold ? FontStyle.Bold : FontStyle.Regular))
+            {
+                g.DrawString(text, font, Brushes.Black, START_X, currentY);
+                // Avança ~15px para a próxima linha (pode ajustar conforme necessidade)
+                currentY += 15f;
+            }
+            return currentY;
+        }
+
+        /// <summary>
+        /// Desenha texto que pode precisar ser quebrado em duas linhas (ex.: Nome longo).
+        /// Retorna a nova posição Y (abaixo da última linha desenhada).
+        /// </summary>
+        private float DrawWrappedLine(Graphics g, string prefix, string text, float currentY)
+        {
+            // Define o limite de 20 chars, como no seu código original
+            const int LIMIT = 20;
+
+            if (text.Length <= LIMIT)
+            {
+                // Pode caber em uma linha
+                currentY = DrawLine(g, prefix + text, currentY, 8, textIsBold(prefix));
+            }
+            else
+            {
+                // Divide em duas linhas
+                string firstPart = text.Substring(0, LIMIT);
+                string secondPart = text.Substring(LIMIT);
+
+                // Primeira linha (com prefixo)
+                currentY = DrawLine(g, prefix + firstPart, currentY, 8, textIsBold(prefix));
+
+                // Segunda linha (sem prefixo)
+                currentY = DrawLine(g, secondPart, currentY, 8, textIsBold(prefix));
+            }
+            return currentY;
+        }
+
+        /// <summary>
+        /// Decide se o texto deve ser negrito ou não, baseado no prefixo.
+        /// (No código original, "Nome" era impresso em negrito, mas "Filiação" não necessariamente.)
+        /// Ajuste conforme sua necessidade.
+        /// </summary>
+        private bool textIsBold(string prefix)
+        {
+            // Se o prefixo for "Nome: ", retorna true (negrito); senão false
+            return prefix.IndexOf("Nome", StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        /// <summary>
+        /// Faz a requisição web para buscar o paciente via JSON.
+        /// </summary>
+        private Paciente FetchPacienteData(string rh)
+        {
+            string url = "http://10.48.17.99:5003/hspmsgh-api/pacientes/consulta/" + rh;
+            WebRequest request = WebRequest.Create(url);
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<Paciente>(result);
+            }
+        }
+
+        /// <summary>
+        /// Combina o nome social com o nome do paciente, se existir.
+        /// </summary>
+        private string BuildNomeCompleto(string nome, string nomeSocial)
+        {
+            if (!string.IsNullOrEmpty(nomeSocial))
+            {
+                // Formato "Social (Original)"
+                return nomeSocial + " (" + nome + ")";
+            }
+            return nome;
+        }
+
+        #endregion
+
+        #region TextBox Events
 
         private void txbRh_KeyPress(object sender, KeyPressEventArgs e)
         {
-
             if (e.KeyChar == (char)Keys.Enter)
             {
-
                 btImprimir_Click(sender, e);
-
             }
         }
 
-       
+        #endregion
     }
 }
